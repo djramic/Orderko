dc6c6dd686e2c56184f5d2d2267f25e995bab733
Rama <djramic@gmail.com>
2020-03-17 12:39:38 +0100

    ADDED: xml for table fragment

--- app/src/main/java/com/example/orderko/TableFragment.java
+++ app/src/main/java/com/example/orderko/TableFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +14,28 @@ import androidx.fragment.app.Fragment;
 
 public class TableFragment extends Fragment {
 
+    private NumberPicker numberPicker;
+    private ImageButton pick_table_imbt;
+    private TextView table_num_txvw;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_table, container, false);
+       View v = inflater.inflate(R.layout.fragment_table, container, false);
+
+       numberPicker = v.findViewById(R.id.numberPicker);
+       pick_table_imbt = v.findViewById(R.id.pick_table_imbt);
+       table_num_txvw = v.findViewById(R.id.table_num_txvw);
+       numberPicker.setMinValue(0);
+       numberPicker.setMaxValue(10);
+
+       pick_table_imbt.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+                int table_nubmer = numberPicker.getValue();
+                table_num_txvw.setText(String.valueOf(table_nubmer));
+           }
+       });
+
+        return v;
     }
 }
