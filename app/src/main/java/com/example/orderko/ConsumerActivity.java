7e53a6f920bbb744cf222dcc1c86553c1cd65e60
Rama <djramic@gmail.com>
2020-03-27 22:34:46 +0100

    WORKING ON: location listener, leave table

--- app/src/main/java/com/example/orderko/ConsumerActivity.java
+++ app/src/main/java/com/example/orderko/ConsumerActivity.java
@@ -2,12 +2,18 @@ package com.example.orderko;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -15,6 +21,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.common.data.DataBufferObserver;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -30,8 +37,10 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
-public class ConsumerActivity extends AppCompatActivity {
+public class ConsumerActivity extends AppCompatActivity{
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private BottomNavigationView bottomNavigationView;
     private Drink drink;
@@ -48,6 +57,12 @@ public class ConsumerActivity extends AppCompatActivity {
     private DrinkListFragment drinkListFragment;
     private BillFragment billFragment;
     private TextView club_info;
+    private float[] dist;
+    private LocationListener locationListenerGPS;
+    private LocationManager locationManager;
+    private Location bello;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,16 +73,25 @@ public class ConsumerActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         tableRef = database.getReference(user.getClub() + "/tables");
 
+        bello = new Location("");
+        bello.setLatitude(45.25848);
+        bello.setLongitude(19.84736);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000,
+                20, locationListenerGPS);
+
         table_fragment = new TableFragment();
         drinkListFragment = new DrinkListFragment();
         billFragment = new BillFragment();
-
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         user_bill_txtv = findViewById(R.id.user_all_bill);
         leave_club_button = findViewById(R.id.leave_table_but);
         table_bill_txtv = findViewById(R.id.table_bill);
         club_info = findViewById(R.id.club_info_txtv);
+
 
 
         db.collection("Clubs")
@@ -240,9 +264,50 @@ public class ConsumerActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("destorsdfk","usao sam u destory");
+    private void mLocationListener(){
+        locationListenerGPS = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                checkDistance(location,bello);
+                Log.d("locationtest","Udaljenost od bella je: " + dist[0]);
+                if(dist[0]/50 >1) {
+                    Log.d("locationtest","Napustam sto" + dist[0]);
+
+                    if(user.getTable() != null) {
+                        leave_table();
+                    }
+                    user.setUserBill("0");
+                    user.setUserLastBill("0");
+                    user.setTable(null);
+                    user.setClub(null);
+
+                    userDb.insertData("0","0",null,"0",null, user.getTableBill());
+                    startActivity(new Intent(ConsumerActivity.this,MainActivity.class));
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
     }
+
+    private void checkDistance(Location location1, Location location2){
+        dist = new float[1];
+        Location.distanceBetween(location1.getLatitude(),location1.getLongitude(),
+                location2.getLatitude(),location2.getLongitude(),dist);
+    }
+
 }
--- app/src/main/java/com/example/orderko/MainActivity.java
+++ app/src/main/java/com/example/orderko/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.orderko;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.protobuf.DescriptorProtos;
 
 import java.util.ArrayList;
 
@@ -18,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView autoCompleteTextView;
     private Button choose_but;
     private User user;
+    private LocationManager locationManager;
+    private Location bello;
+    private LocationListener locationListenerGPS;
+    private float[] dist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +46,77 @@ public class MainActivity extends AppCompatActivity {
         user = User.getInstance();
         initData();
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,clubs);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000,
+                20, locationListenerGPS);
+
+
+
+        dist = new float[1];
+        bello = new Location("");
+        bello.setLatitude(45.25848);
+        bello.setLongitude(19.84736);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, clubs);
         autoCompleteTextView.setThreshold(1);
         autoCompleteTextView.setAdapter(adapter);
 
         choose_but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(clubs.contains(autoCompleteTextView.getText().toString())){
+                if (clubs.contains(autoCompleteTextView.getText().toString())) {
                     user.setClub(autoCompleteTextView.getText().toString());
-                    startActivity(new Intent(MainActivity.this,ConsumerActivity.class));
+                    Log.d("locationtest","rastojanje je " + String.valueOf(dist[0]));
+
+                    if(dist[0] / 50 < 1){
+                        locationManager.removeUpdates(locationListenerGPS);
+                        locationManager = null;
+
+                        startActivity(new Intent(MainActivity.this, ConsumerActivity.class));
+                    }else {
+                        Toast.makeText(MainActivity.this,"Morate biti u blizini lokala",Toast.LENGTH_LONG).show();
+                    }
+
                 }
             }
         });
 
+
+    }
+
+    private void checkDistance(Location location1, Location location2){
+        dist = new float[1];
+        Location.distanceBetween(location1.getLatitude(),location1.getLongitude(),
+                location2.getLatitude(),location2.getLongitude(),dist);
+    }
+
+    private void mLocationListener(){
+        locationListenerGPS = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                checkDistance(location,bello);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
     }
 
     private void initData(){
@@ -49,5 +124,8 @@ public class MainActivity extends AppCompatActivity {
         clubs.add("Rama bar");
         clubs.add("Polemika");
     }
+
+
+
 
 }
